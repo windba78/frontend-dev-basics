08cdfaf3e93095edb95fada014d4acd85751aa8a
HEE-JE <windba78@naver.com>
2022-07-05 12:18:33 +0900

    Browser programming jQuery #3

---
+++ ch07/src/main/webapp/jquery/jquery-simple.js
@@ -0,0 +1,39 @@
+/**
+	간단한 jQuery 구현 해보기
+ */
+
+var jQuery = function(param) {
+	if (typeof (param) === 'function') {
+		window.addEventListener("load", param);
+		return;
+	}
+
+	if (typeof (param) === 'string') {
+		var elements = document.querySelectorAll(param);
+		return new _jQuery(elements);
+	}
+}
+
+var _jQuery = function(elements) {
+	this.length = elements.length;
+	for (var i = 0; i < this.length; i++) {
+		this[i] = elements[i];
+	}
+}
+
+_jQuery.prototype.jquery = 'jquery-simple.0.0.1';
+_jQuery.prototype.css = function(name, value) {
+	for (var i = 0; i < this.length; i++) {
+		this[i].style[name] = value;
+	}
+	return this;
+};
+
+_jQuery.prototype.click = function(handler) {
+	for (var i = 0; i < this.length; i++) {
+		this[i].addEventListener("click", handler);
+	}
+	return this;
+};
+
+var $ = jQuery;
