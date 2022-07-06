52ff52083b05880d248d9e11b1f36c9737199aa3
HEE-JE <windba78@naver.com>
2022-07-06 11:08:49 +0900

    Browser programming jQuery #7

--- ch07/src/main/webapp/image-viewer.js
+++ ch07/src/main/webapp/image-viewer.js
@@ -5,9 +5,27 @@ var imageViewer = {
 	_init: function() {
 		$("#btn-change").click(this._changeImage.bind(this));
 		$(".image-viewer img").dblclick(this._showImageName);
+		$("#btn-slideshow").click(this._slideShow.bind(this));
 
 		// 첫번째 이미지 선택
 		this._changeImage();
+	},
+	_slideShow: function() {
+		if (this.intervalId) {
+			/* 슬라이드쇼가 진행 상태 */
+			// 1. 타이머 중지
+			clearInterval(this.intervalId);
+			this.intervalId = null;
+			// 2. 버튼 텍스트: 시작 -> 중지
+			$("#btn-slideshow").text("슬라이드쇼 시작");
+		} else {
+			/* 슬라이드쇼가 중지 상태 */
+			// 1. 타이머 시작
+			this.intervalId = setInterval(this._changeImage.bind(this), 2000);
+			// 2. 버튼 텍스트: 중지 -> 시작
+			$("#btn-slideshow").text("슬라이드쇼 중지");
+		}
+		intervalId = null;
 	},
 	_showImageName: function() {
 		alert($(this).attr('alt'));
